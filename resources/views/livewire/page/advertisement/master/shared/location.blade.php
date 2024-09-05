9230cafb2673aa8e614a4e14c69a368658f6747f
Sergey Vilkov <7921920@gmail.com>
2024-09-05 17:30:29 +0300

    Меняет текст

--- resources/views/livewire/page/advertisement/master/shared/location.blade.php
+++ resources/views/livewire/page/advertisement/master/shared/location.blade.php
@@ -1,14 +1,14 @@
 <div class="p-3 rounded-4 bg-light mb-3">
-    <h2 class="mb-0">Укажите где вы хотите получить услуги</h2>
+    <h2 class="mb-0">Укажите где вы предоставляете услуги</h2>
 
     @error('locations')
     <div class="alert alert-danger my-2">{{ $message }}</div>
     @enderror
 
     <livewire:components.multi-select
-            :service="$serviceLocation"
-            @change-selected="setLocations($event.detail.selected)"
-            :set="$locations"
-            placeholder="Укажите места работы"
+        :service="$serviceLocation"
+        @change-selected="setLocations($event.detail.selected)"
+        :set="$locations"
+        placeholder="Укажите места работы"
     />
 </div>
