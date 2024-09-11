0149886019d7a372e56fc375fd789accce4850e0
Sergey Vilkov <7921920@gmail.com>
2024-09-11 14:15:17 +0300

    feat: Добавляет тарифы для выведения в top

--- database/seeders/TopTariffsSeeder.php
+++ database/seeders/TopTariffsSeeder.php
@@ -15,39 +15,215 @@ class TopTariffsSeeder extends Seeder
     public function run(): void
     {
         AdvertisementTopTariff::query()->create([
-            'name'       => '1 день',
+            'name' => '1 день',
             'count_days' => 1,
-            'type'       => AdvertisementTopTariffsType::Minute,
-            'minutes'    => 60,
-            'status'     => true,
-            'price'      => 100,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 100,
         ]);
 
         AdvertisementTopTariff::query()->create([
-            'name'       => '1 день PRO',
+            'name' => '1 день PRO',
             'count_days' => 1,
-            'type'       => AdvertisementTopTariffsType::Minute,
-            'minutes'    => 15,
-            'status'     => true,
-            'price'      => 300,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 300,
         ]);
 
         AdvertisementTopTariff::query()->create([
-            'name'       => '1 день PRO +',
+            'name' => '1 день PREMIUM',
             'count_days' => 1,
-            'type'       => AdvertisementTopTariffsType::Minute,
-            'minutes'    => 1,
-            'status'     => true,
-            'price'      => 1000,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 1,
+            'status' => true,
+            'price' => 1000,
         ]);
 
+        // Неделя
         AdvertisementTopTariff::query()->create([
-            'name'       => '1 неделя',
+            'name' => '1 неделя',
             'count_days' => 7,
-            'type'       => AdvertisementTopTariffsType::ConcreteTime,
+            'type' => AdvertisementTopTariffsType::ConcreteTime,
             'start_time' => '00:00:00',
-            'status'     => true,
-            'price'      => 1000,
+            'status' => true,
+            'price' => 300,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 неделя каждые 6 часов',
+            'count_days' => 7,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 360,
+            'status' => true,
+            'price' => 800,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 неделя PRO',
+            'count_days' => 7,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 1600,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 неделя PREMIUM',
+            'count_days' => 7,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 6000,
+        ]);
+
+        // Месяц
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц',
+            'count_days' => 30,
+            'type' => AdvertisementTopTariffsType::ConcreteTime,
+            'start_time' => '00:00:00',
+            'status' => true,
+            'price' => 1000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц каждые 6 часов',
+            'count_days' => 30,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 360,
+            'status' => true,
+            'price' => 3000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PRO',
+            'count_days' => 30,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 6000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PREMIUM',
+            'count_days' => 30,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 25000,
+        ]);
+
+        // 3 Месяца
+        AdvertisementTopTariff::query()->create([
+            'name' => '3 месяц',
+            'count_days' => 91,
+            'type' => AdvertisementTopTariffsType::ConcreteTime,
+            'start_time' => '00:00:00',
+            'status' => true,
+            'price' => 2000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '3 месяца каждые 6 часов',
+            'count_days' => 91,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 360,
+            'status' => true,
+            'price' => 6000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '3 месяца PRO',
+            'count_days' => 91,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 12000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '3 месяца PREMIUM',
+            'count_days' => 91,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 50000,
+        ]);
+
+        // 6 Месяцев
+        AdvertisementTopTariff::query()->create([
+            'name' => 'Полгода',
+            'count_days' => 183,
+            'type' => AdvertisementTopTariffsType::ConcreteTime,
+            'start_time' => '00:00:00',
+            'status' => true,
+            'price' => 4000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => 'Полгода каждые 6 часов',
+            'count_days' => 183,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 360,
+            'status' => true,
+            'price' => 12000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PRO',
+            'count_days' => 183,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 24000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PREMIUM',
+            'count_days' => 183,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 100000,
+        ]);
+
+        // Год
+        AdvertisementTopTariff::query()->create([
+            'name' => 'Полгода',
+            'count_days' => 366,
+            'type' => AdvertisementTopTariffsType::ConcreteTime,
+            'start_time' => '00:00:00',
+            'status' => true,
+            'price' => 6000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => 'Полгода каждые 6 часов',
+            'count_days' => 366,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 360,
+            'status' => true,
+            'price' => 20000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PRO',
+            'count_days' => 366,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 60,
+            'status' => true,
+            'price' => 40000,
+        ]);
+
+        AdvertisementTopTariff::query()->create([
+            'name' => '1 месяц PREMIUM',
+            'count_days' => 366,
+            'type' => AdvertisementTopTariffsType::Minute,
+            'minutes' => 15,
+            'status' => true,
+            'price' => 180000,
         ]);
     }
 
