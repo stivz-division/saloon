6b695a1754be0e3a83edafbaf7eb367e8cec13dd
Sergey Vilkov <7921920@gmail.com>
2024-10-11 17:19:23 +0400

    feat: Добавляет реквизиты

--- resources/views/livewire/page/tariff-page.blade.php
+++ resources/views/livewire/page/tariff-page.blade.php
@@ -29,14 +29,14 @@
                            {{ $tariff->stock->price }}₽
                         </span>
                         <small
-                                class="text-body-secondary text-decoration-line-through fw-light"
-                                style="font-size: 20px"
+                            class="text-body-secondary text-decoration-line-through fw-light"
+                            style="font-size: 20px"
                         >
                             {{$tariff->price}}₽
                         </small>
                         <span
-                                class="text-danger"
-                                style="font-size: 20px"
+                            class="text-danger"
+                            style="font-size: 20px"
                         >
                     &nbsp;      -{{ $tariff->stock->percent }}%
                         </span>
@@ -47,4 +47,11 @@
         @endforeach
         </tbody>
     </table>
+    <div class="bg-dark text-white py-1 mt-auto">
+        <div class="container text-center text-nowrap">
+            <p class="mb-0">ИНН: 781129398703 | Вилков Сергей Андреевич | Email: 9219201792@mail.ru | Тел.: +7 921
+                9201792 |
+                Адрес: 194017, г.Санкт-Петербург, ул.Калязинская 7-30</p>
+        </div>
+    </div>
 </div>
