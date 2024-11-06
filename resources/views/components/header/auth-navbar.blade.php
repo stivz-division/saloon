832ba443c22a52a8f8e1de40fc5dfb8a4d25b8a0
Sergey Vilkov <7921920@gmail.com>
2024-11-06 16:57:30 +0400

    fix: Убирает тарифы из меню

--- resources/views/components/header/auth-navbar.blade.php
+++ resources/views/components/header/auth-navbar.blade.php
@@ -2,33 +2,33 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a
-                    class="navbar-brand"
-                    href="{{ route('welcome') }}"
+                class="navbar-brand"
+                href="{{ route('welcome') }}"
             >
                 <img
-                        src="{{ Vite::asset('resources/images/Scottie_glasses.png') }}"
-                        class="img-fluid"
-                        style="max-height: 40px;"
-                        alt="LOGO"
+                    src="{{ Vite::asset('resources/images/Scottie_glasses.png') }}"
+                    class="img-fluid"
+                    style="max-height: 40px;"
+                    alt="LOGO"
                 >
                 <span class="badge text-bg-primary">
                     {{ auth()->user()->account_type->name() }}
                 </span>
             </a>
             <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
             >
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div
-                    class="collapse navbar-collapse justify-content-end"
-                    id="navbarSupportedContent"
+                class="collapse navbar-collapse justify-content-end"
+                id="navbarSupportedContent"
             >
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
@@ -51,14 +51,14 @@
                         <x-header.item route="profile">Профиль</x-header.item>
                     </li>
 
-                    <li class="nav-item">
-                        <x-header.item route="tariff">Тарифы</x-header.item>
-                    </li>
+                    {{--                    <li class="nav-item">--}}
+                    {{--                        <x-header.item route="tariff">Тарифы</x-header.item>--}}
+                    {{--                    </li>--}}
 
                 </ul>
                 <form
-                        method="post"
-                        action="{{ route('logout') }}"
+                    method="post"
+                    action="{{ route('logout') }}"
                 >
                     @csrf
                     @if(auth()->user()->isClient())
