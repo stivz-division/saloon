c7040d7db6010139bee09e9c7a84885c606e0380
Sergey Vilkov <7921920@gmail.com>
2025-04-17 18:43:36 +0400

    BugFix добавляет в профиль slider

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.scss', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.scss',
+                'resources/js/app.js',
+                'resources/js/profile/master/advertisements-slider.js'
+            ],
+            refresh: true,
+        }),
+    ],
 });
